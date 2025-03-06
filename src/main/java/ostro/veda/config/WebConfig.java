ff54d0d3340946c6f42e513fa08391cfb19ee27e
João Carlos Roveda Ostrovski <ojcroveda@gmail.com>
2025-03-05 22:20:58 -0300

    Spring Boot Web implementation

--- src/main/java/ostro/veda/config/WebConfig.java
+++ src/main/java/ostro/veda/config/WebConfig.java
@@ -1,35 +1,16 @@
 package ostro.veda.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
-@EnableWebMvc
-@Configuration
-@ComponentScan({"controllers"})
 public class WebConfig implements WebMvcConfigurer {
 
-    // == constants ==
-    public static final String RESOLVER_PREFIX = "/WEB-INF/view/";
-    public static final String RESOLVER_SUFFIX =".jsp";
-
-    // == bean methods
     @Bean
-    public ViewResolver viewResolver() {
-        UrlBasedViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix(RESOLVER_PREFIX);
-        viewResolver.setSuffix(RESOLVER_SUFFIX);
-        return viewResolver;
-    }
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("home");
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/view/"); // Location of JSP files
+        resolver.setSuffix(".jsp");          // File extension for JSP files
+        return resolver;
     }
 }
--- src/main/java/ostro/veda/controller/MainController.java
+++ src/main/java/ostro/veda/controller/MainController.java
@@ -1,18 +1,28 @@
 package ostro.veda.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String root() {
         return "redirect:/home";
     }
 
-    @RequestMapping("/home")
+    @GetMapping("/home")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+
+    @GetMapping("/error")
+    public String error() {
+        return "error";
     }
 }
